37a301d45515540d99d36e48bb52f418c6e3e65b
tarun-chaudhary1 <tarunchaudhary9911@gmail.com>
2025-09-04 15:40:02 +0530

    Init

---
+++ src/WrapperClasses/Demo.java
@@ -0,0 +1,19 @@
+package WrapperClasses;
+
+public class Demo {
+    public static void main(String[] args) {
+        Integer i= Integer.valueOf(10);
+        System.out.println(i);
+
+
+        int i1=Integer.parseInt("342");
+        System.out.println(i1);
+        Double i2=3.32;
+
+
+        Integer a=1000;
+        Integer b=1000;
+
+        System.out.println(a==b);
+    }
+}
